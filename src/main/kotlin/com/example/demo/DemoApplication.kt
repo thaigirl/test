70fbd375396e68d6edd4a122c6947e28980cb249
basicfu <basicfu@gmail.com>
2018-05-20 16:41:05 +0800

    我要和入 (#4)
    * 这是一个新的提交

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -7,5 +7,7 @@ import org.springframework.boot.runApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
+    println("1234")
+    println("46")
     runApplication<DemoApplication>(*args)
 }
